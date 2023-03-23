42741e0dbf4ddd8b808b22850d4a8507ef03a168
David Martins <david.guimaraes@portalsesisp.org.br>
2023-03-22 21:49:30 -0300

    Adicionado autorização para API ser publica

--- back/src/routes.js
+++ back/src/routes.js
@@ -4,7 +4,14 @@ const router = require('express').Router();
 
 //import controllers
 const UserCeontroller = require('./controllers/UserController')
-
+//permite que a api fique publica para ser acessada pelas aplicações
+const UserCeontroller = require('./controllers/UserController')
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+//rotas da tabela users
 router.post('/create', UserCeontroller.create);
 router.post('/login', UserCeontroller.login);
 
